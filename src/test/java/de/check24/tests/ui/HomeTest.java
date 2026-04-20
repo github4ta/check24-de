14173bf06326669a973d76c9b6d4004debaa8f3c
mtsimashenka <maksim-timoshenko-97@mail.ru>
2026-04-20 10:07:24 +0300

    VS-005 Verify CHECK24 Logo visibility and functionality (#27)
    VS-005 Verify CHECK24 Logo visibility and functionality

--- src/test/java/de/check24/tests/ui/HomeTest.java
+++ src/test/java/de/check24/tests/ui/HomeTest.java
@@ -242,6 +242,17 @@ public class HomeTest extends BaseUITest {
     }
 
     @Test
+    public void testVS005() {
+        homePage.navigateToHomePage();
+        homePage.clickCookieAcceptButton();
+        homePage.clickLinkFerienwohnung();
+        homePage.clickLogoCheckInHeader();
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.check24.de/";
+        Assertions.assertEquals(expectedUrl, actualUrl);
+    }
+
+    @Test
     public void testVS003() {
         homePage.navigateToHomePage();
         homePage.clickCookieAcceptButton();
@@ -272,8 +283,8 @@ public class HomeTest extends BaseUITest {
                 "URL не содержит 'passwort-vergessen'. Текущий URL: " + driver.getCurrentUrl());
     }
 
-@ Test
-    public void testVM004 () {
+    @Test
+    public void testVM004() {
         homePage.navigateToHomePage();
         homePage.clickAGBlink();
         String AGBUrl = driver.getCurrentUrl();
@@ -352,7 +363,7 @@ public class HomeTest extends BaseUITest {
     }
 
     @Test
-    public void testVS011 () {
+    public void testVS011() {
         homePage.navigateToHomePage();
         homePage.clickReiseButton();
         String reiseUrl = driver.getCurrentUrl();
