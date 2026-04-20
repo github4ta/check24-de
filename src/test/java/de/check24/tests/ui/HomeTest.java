c4d7ea4e5fa312d4960e3a8332a02e782e1bc195
mtsimashenka <maksim-timoshenko-97@mail.ru>
2026-04-20 09:34:03 +0300

    AU001 - Authorization with valid data is working correctly
    added package util for shadow waits and add LoginTest for checking

--- src/test/java/de/check24/tests/ui/HomeTest.java
+++ src/test/java/de/check24/tests/ui/HomeTest.java
@@ -265,7 +265,7 @@ public class HomeTest extends BaseUITest {
         homePage.clickCookieAcceptButton();
 
         homePage.clickLoginIcon();
-        homePage.enterEmail(EXISTING_EMAIL);
+        homePage.enterEmail();
         homePage.clickForgotPassword();
 
         Assertions.assertTrue(driver.getCurrentUrl().contains("passwort-vergessen"),
@@ -305,6 +305,8 @@ public class HomeTest extends BaseUITest {
         Assertions.assertTrue(
                 actual.contains("https://www.check24.de/suche/?q=paris"),
                 "Unexpected URL: " + actual);
+    }
+
     @Test
     void testVM005() {
         homePage.navigateToHomePage();
