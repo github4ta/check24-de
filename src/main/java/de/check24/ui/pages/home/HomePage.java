bdca958ebf359bdc4112431858ac7048b68176e9
sergei.tsarik <serge.tsarik@gmail.com>
2026-04-24 15:00:18 +0200

    add search code

--- src/main/java/de/check24/ui/pages/home/HomePage.java
+++ src/main/java/de/check24/ui/pages/home/HomePage.java
@@ -1,5 +1,6 @@
 package de.check24.ui.pages.home;
 
+import de.check24.ui.driver.Driver;
 import de.check24.ui.pages.base.BasePage;
 
 import static de.check24.ui.driver.Driver.getText;
@@ -10,6 +11,7 @@ public class HomePage extends BasePage {
     private final String FOOTER_COMPANY_LINE = "//div[@class='c24-footer-company-line']";
     private final String LABEL_AMNELDEN = "//div[@class='c24-customer-icon c24-customer-icon-lo c24-icon']";
     private final String LABEL_AKTIVITATEN = "//a[@class='c24-activities-icon c24-header-hover']";
+    private final String QUICK_CHIPS_LINK_HOTEL = "//a[@data-identifier='hotel']";
 
     public String getFooterCompanyLine() {
         return getText(FOOTER_COMPANY_LINE);
@@ -21,5 +23,9 @@ public class HomePage extends BasePage {
 
     public boolean isLabelAktivitätenDisplayed() {
         return isElementDisplayedWithWait(LABEL_AKTIVITATEN, 5);
+    }
+
+    public void clickQuickChipsLinkHotel() {
+        Driver.click(QUICK_CHIPS_LINK_HOTEL);
     }
 }
