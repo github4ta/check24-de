6d323281ecd3c13fac8e9e4af4d603a5fd98aeec
JakovJL <mr.nalegach@gmail.com>
2026-06-19 15:01:38 +0300

    add login validation UI tests (UI-TC-001..004)

---
+++ src/main/java/com/mytheresa/ui/BaseMytheresaPage.java
@@ -0,0 +1,12 @@
+package com.mytheresa.ui;
+
+/**
+ * @author Sergei Tsarik, Maria Ramanova, Nalegach Yakov
+ */
+public class BaseMytheresaPage {
+
+    private final String basePage = "https://www.mytheresa.com/";
+
+
+
+}
