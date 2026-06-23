359e58040aef3cfe9a8b96ee9666eecf06e06798
JakovJL <mr.nalegach@gmail.com>
2026-06-23 18:02:58 +0300

    merge commit TC-004 to main

--- src/test/java/com/mytheresa/WelcomePageTest.java
+++ src/test/java/com/mytheresa/WelcomePageTest.java
@@ -4,6 +4,7 @@ import com.mytheresa.ui.AuthPage;
 import jdk.jfr.Name;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,6 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.SecureRandom;
 import java.time.Duration;
 
 public class WelcomePageTest extends AuthPage {
@@ -109,25 +111,6 @@ public class WelcomePageTest extends AuthPage {
             log.info("Country selection modal failed. {}", exception.getMessage());
         }
     }
-import com.mytheresa.ui.AuthPage;
-import jdk.jfr.Name;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.security.SecureRandom;
-import java.time.Duration;
-
-public class WelcomePageTest extends AuthPage {
-
-    private static final Logger log = LoggerFactory.getLogger(WelcomePageTest.class);
 
     @Test
     @Name("UI-TC-004: Successful login with valid credentials")
@@ -190,50 +173,5 @@ public class WelcomePageTest extends AuthPage {
         log.info("Login submitted");
 
         driver.quit();
-    }
-
-    private void acceptCookies(WebDriver driver) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(getShadowHostLocator()))
-                .getShadowRoot()
-                .findElement(getAcceptCookiesButton())
-                .click();
-    }
-
-    private WebDriver initDriver() throws InterruptedException {
-        ChromeOptions options = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get(getLOGIN_URL());
-        log.info("We on {}", driver.getCurrentUrl());
-        Thread.sleep(1000);
-        return driver;
-    }
-
-    public void passUserPreferences(WebDriver driver) {
-
-        try {
-            log.info("Opening country selection modal");
-            driver.findElement(getModalWrapperCountrySelectionButton()).click();
-            Thread.sleep(1000);
-
-            log.info("Clicking search field");
-            driver.findElement(getSearchCountryInput()).click();
-            Thread.sleep(300);
-
-            log.info("Searching for GB");
-            driver.findElement(getSearchCountryInput()).sendKeys("United");
-            Thread.sleep(500);
-
-            log.info("Selecting GB from results");
-            driver.findElement(getCountryUnitedKingdom()).click();
-            Thread.sleep(500);
-
-            log.info("Closing modal save button");
-            driver.findElement(getModalWrapperSaveButton()).click();
-
-        } catch (Exception exception) {
-            log.info("Country selection modal failed. {}", exception.getMessage());
-        }
     }
 }
