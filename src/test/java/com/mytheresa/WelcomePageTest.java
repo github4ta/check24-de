7177f17bd690a9f9a3988c7a58b6872bb0ea5a21
JakovJL <mr.nalegach@gmail.com>
2026-06-23 18:08:39 +0300

    merge commit TC-003 to main

--- src/test/java/com/mytheresa/WelcomePageTest.java
+++ src/test/java/com/mytheresa/WelcomePageTest.java
@@ -174,23 +174,6 @@ public class WelcomePageTest extends AuthPage {
 
         driver.quit();
     }
-import com.mytheresa.ui.AuthPage;
-import jdk.jfr.Name;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
-
-public class WelcomePageTest extends AuthPage {
-
-    private static final Logger log = LoggerFactory.getLogger(WelcomePageTest.class);
 
     @Test
     @Name("UI-TC-003: Validation error when submitting valid Email and empty Password")
@@ -208,23 +191,5 @@ public class WelcomePageTest extends AuthPage {
         Assertions.assertEquals("Required field", driver.findElement(getPasswordError()).getText());
 
         driver.quit();
-    }
-
-    private void acceptCookies(WebDriver driver) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(getShadowHostLocator()))
-                .getShadowRoot()
-                .findElement(getAcceptCookiesButton())
-                .click();
-    }
-
-    private WebDriver initDriver() throws InterruptedException {
-        ChromeOptions options = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get(getLOGIN_URL());
-        log.info("We on {}", driver.getCurrentUrl());
-        Thread.sleep(1000);
-        return driver;
     }
 }
