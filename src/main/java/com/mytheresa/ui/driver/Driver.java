05432fed239cd188d4fdde81471d6658fdfad03f
sergei.tsarik <serge.tsarik@gmail.com>
2026-06-24 12:40:11 +0200

    add some code

--- src/main/java/com/mytheresa/ui/driver/Driver.java
+++ src/main/java/com/mytheresa/ui/driver/Driver.java
@@ -1,6 +1,5 @@
 package com.mytheresa.ui.driver;
 
-import de.check24.config.ConfigLoader;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -30,8 +29,8 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            String browserType = ConfigLoader.get("browser.type", "chrome");
-            driver = DriverFactory.createDriver(browserType);
+            //String browserType = ConfigLoader.get("browser.type", "chrome");
+            //driver = DriverFactory.createDriver(browserType);
             driver.manage().window().maximize();
         }
         return driver;
