489534182deb0bb7f44c0c32562b2b6f217b5311
MaryiaRamanava <romanova998ma@gmail.com>
2026-07-06 10:37:52 +0300

    refactor

--- src/main/java/com/mytheresa/ui/page/AuthPage.java
+++ src/main/java/com/mytheresa/ui/page/AuthPage.java
@@ -2,6 +2,7 @@ package com.mytheresa.ui.page;
 
 import com.mytheresa.ui.page.base.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AuthPage extends BasePage {
@@ -10,52 +11,53 @@ public class AuthPage extends BasePage {
         super();
     }
 
-    private String LOGIN_URL = "https://www.mytheresa.com/int/en/account/login";
-    private String ALREADY_REGISTERED_TEXT = "//div[@class='login__signin__title']";
-    private String EMAIL_INPUT = "//input[@name ='email']";
-    private String PASSWORD_INPUT = "//input[@name='password']";
-    private String LOGIN = "//div[@class='button']";
-    private String EMAIL_ERROR_MESSAGE = "//div[@id='email__error']";
-    private String PASSWORD_ERROR_MESSAGE = "//div[@id='password__error']";
+    private final String loginPageUrl = "https://www.mytheresa.com/int/en/account/login";
+
+    private final WebElement emailInput = driver.findElement(By.xpath("//input[@name ='email']"));
+    private final WebElement passwordInput = driver.findElement(By.xpath("//input[@name ='password']"));
+    private final WebElement emailErrorText = driver.findElement(By.xpath("//div[@id ='email__error']"));
+    private final WebElement passwordErrorText = driver.findElement(By.xpath("//div[@id='password__error']"));
+    private final WebElement loginButton = driver.findElement(By.xpath("//div[@class='button']"));
+    private final WebElement alreadyRegisteredText = driver.findElement(By.xpath("//div[@class='login__signin__title']"));
 
     public AuthPage goToLoginPage(){
-        driver.get(LOGIN_URL);
+        driver.get(loginPageUrl);
         return this;
     }
+
+    public AuthPage setEmailInput(String email) {
+        emailInput.sendKeys(email);
+        return this;
+    }
+    public AuthPage setPasswordInput(String password) {
+        passwordInput.sendKeys(password);
+        return this;
+    }
+
     public String getAlreadyRegisteredText(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ALREADY_REGISTERED_TEXT))).getText();
+        return wait.until(ExpectedConditions.visibilityOf(alreadyRegisteredText)).getText();
     }
 
     public AuthPage clickToLoginButton(){
-        driver.findElement(By.xpath(LOGIN)).click();
-        return this;
-    }
-
-    public AuthPage inputEmail(String email){
-        driver.findElement(By.xpath(EMAIL_INPUT)).sendKeys(email);
-        return this;
-    }
-
-    public AuthPage inputPassword(String password){
-        driver.findElement(By.xpath(PASSWORD_INPUT)).sendKeys(password);
+        loginButton.click();
         return this;
     }
 
     public AuthPage clickToEmailField(){
-        driver.findElement(By.xpath(EMAIL_INPUT)).click();
+        emailInput.click();
         return this;
     }
 
     public AuthPage clickToPasswordField(){
-        driver.findElement(By.xpath(PASSWORD_INPUT)).click();
+        passwordInput.click();
         return this;
     }
 
     public String getEmailErrorMessage(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EMAIL_ERROR_MESSAGE))).getText();
+        return wait.until(ExpectedConditions.visibilityOf(emailErrorText)).getText();
     }
 
     public String getPasswordErrorMessage(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PASSWORD_ERROR_MESSAGE))).getText();
+        return wait.until(ExpectedConditions.visibilityOf(passwordErrorText)).getText();
     }
 }
