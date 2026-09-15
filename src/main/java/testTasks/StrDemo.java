fc3021d75678eee242de64d73e06fc8ab008a327
apelsin2281337 <ggwp.gg6578@gmail.com>
2026-09-15 17:38:44 +0300

    method renamed to avoid conflicts

--- src/main/java/testTasks/StrDemo.java
+++ src/main/java/testTasks/StrDemo.java
@@ -2,7 +2,7 @@ package testTasks;
 
 // Demonstrate escape sequences in strings.
 public class StrDemo {
-    public static void main(String[] args) {
+    public static void stringDemo(String[] args) {
         System.out.println("First line\nSecond line");
         System.out.println("A\tB\tC");
         System.out.println("D\tE\tF") ;
