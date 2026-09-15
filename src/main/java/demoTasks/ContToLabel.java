5f4c69ea84cc2edde9839c2bad7aa3dd58666d66
anna <annakazarez@gmail.com>
2026-09-15 12:43:20 +0300

    add class file

--- src/main/java/demoTasks/ContToLabel.java
+++ src/main/java/demoTasks/ContToLabel.java
@@ -1,4 +1,15 @@
 package demoTasks;
 
 public class ContToLabel {
+    public static void printLabel() {
+        outerloop:
+        for(int i=1; i < 10; i++) {
+            System.out.print("\nOuter loop pass " + i +
+                    ", Inner loop: ");
+            for(int j = 1; j < 10; j++) {
+                if(j == 5) continue outerloop; // continue outer loop
+                System.out.print(j);
+            }
+        }
+    }
 }
