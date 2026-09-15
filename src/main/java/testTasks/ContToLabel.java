23e282919c942ca816d08faa412b5093bacd7107
apelsin2281337 <ggwp.gg6578@gmail.com>
2026-09-15 17:48:26 +0300

    Fixed a wrong class being implemented

---
+++ src/main/java/testTasks/ContToLabel.java
@@ -0,0 +1,16 @@
+package testTasks;
+
+// Use continue with a label.
+class ContToLabel {
+    public static void cont(String args[]) {
+        outerloop:
+        for(int i=1; i < 10; i++) {
+            System.out.print("\nOuter loop pass " + i +
+                    ", Inner loop: ");
+            for(int j = 1; j < 10; j++) {
+                if(j == 5) continue outerloop; // continue outer loop
+                System.out.print(j);
+            }
+        }
+    }
+}
