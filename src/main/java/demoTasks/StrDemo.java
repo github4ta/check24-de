e58a926fd79fca1e83f96cbb4090be4eaf94bfbf
anna <annakazarez@gmail.com>
2026-09-15 10:04:20 +0300

    add class StrDemo page 41

---
+++ src/main/java/demoTasks/StrDemo.java
@@ -0,0 +1,9 @@
+package demoTasks;
+
+class StrDemo {
+    public static void demo() {
+        System.out.println("First line\nSecond line");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF") ;
+    }
+}
