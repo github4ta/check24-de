b889c574cbe7179d54a38fa2a6b716aeac9586f5
anna <annakazarez@gmail.com>
2026-09-16 10:07:00 +0300

    add class MinMax2

---
+++ src/main/java/demoTasks/MinMax2.java
@@ -0,0 +1,15 @@
+package demoTasks;
+
+public class MinMax2 {
+    public static void calculateMinMax() {
+        int[] nums = { 99, -10, 100123, 18, -978,
+                5623, 463, -9, 287, 49 };
+        int min, max;
+        min = max = nums[0];
+        for(int i=1; i < 10; i++) {
+            if(nums[i] < min) min = nums[i];
+            if(nums[i] > max) max = nums[i];
+        }
+        System.out.println("Min and max: " + min + " " + max);
+    }
+}
