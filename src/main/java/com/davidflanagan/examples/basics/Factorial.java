1c536925940d00d5ba24bc8a8dc4bea2359a22aa
Darya <daryarogachova@gmail.com>
2026-09-16 16:42:17 +0300

    add Factorial task 31

---
+++ src/main/java/com/davidflanagan/examples/basics/Factorial.java
@@ -0,0 +1,16 @@
+package com.davidflanagan.examples.basics;
+/**
+ * Этот класс не определяет метод main() и поэтому не является
+ * самостоятельной программой. Он, тем не менее, определяет полезный
+ * метод, который можно использовать в других программах.
+ **/
+ public class Factorial {
+ /** Вычисляем и возвращаем x!, факториал x */
+ public static int factorial(int x) {
+ if (x < 0) throw new IllegalArgumentException("x должен быть >= 0");
+ int fact = 1;
+ for (int i = 2; i <= x; i++) // Цикл
+     fact *= i; // Краткая запись для fact = fact * i;
+ return fact;
+ }
+}
