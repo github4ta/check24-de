0209ba59bc1a42cd59762db76b20afb4d25fb0f4
anna <annakazarez@gmail.com>
2026-09-18 11:59:04 +0300

    refactor method sumArrayElements

--- src/main/java/annanovysh/Code01.java
+++ src/main/java/annanovysh/Code01.java
@@ -5,19 +5,24 @@ import java.util.stream.IntStream;
 public class Code01 {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 3};
-        int result = sumArrayElements(numbers);
+        long result = sumArrayElements(numbers);
         System.out.println("Sum of array elements: " + result);
 
         int[] numbers2 = {};
-        int result2 = sumArrayElements(numbers2);
+        long result2 = sumArrayElements(numbers2);
         System.out.println("Sum of array elements: " + result2);
 
         int[] numbers3 = null;
-        int result3 = sumArrayElements(numbers2);
+        long result3 = sumArrayElements(numbers3);
         System.out.println("Sum of array elements: " + result3);
     }
 
-    public static int sumArrayElements(int[] numbers) {
-        return numbers == null ? 0 : IntStream.of(numbers).sum();
+    public static long sumArrayElements(int[] numbers) {
+        if(numbers == null)
+            return 0;
+        return IntStream.of(numbers).
+                asLongStream().
+                sum();
+
     }
 }
