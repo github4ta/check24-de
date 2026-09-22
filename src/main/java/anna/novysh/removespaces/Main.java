87cddeccaca62f81251bf3bfab8fe4bf4742cc8b
anna <annakazarez@gmail.com>
2026-09-22 12:41:14 +0300

    add method which removing all extra spaces

---
+++ src/main/java/anna/novysh/removespaces/Main.java
@@ -0,0 +1,11 @@
+package anna.novysh.removespaces;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hello      world!\n  How   are you?\b";
+        String cleanText = Utils.removeAllSpaces(text);
+
+        System.out.printf("Initial text is %s%n", text);
+        System.out.printf("Clean (without extra spaces) text is %s ", cleanText);
+    }
+}
