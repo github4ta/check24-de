5a62d42268521b8d11466dbeb5a51ebd777dfb79
anna <annakazarez@gmail.com>
2026-09-22 13:27:56 +0300

    add method which analyse log and find sessionID

---
+++ src/main/java/anna/novysh/parsing/LogParse.java
@@ -0,0 +1,21 @@
+package anna.novysh.parsing;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LogParse {
+    private static final Pattern SESSION_PATTERN = Pattern.compile("SessionID:\\s*'(.*)'");
+
+    public static String extractSessionID(String log) {
+        if (log == null) {
+            return "";
+        }
+        Matcher matcher = SESSION_PATTERN.matcher(log);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return "";
+    }
+}
