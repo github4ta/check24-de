ef5fffbe57b65ca8edf5e770c8d60cbaac47c60c
anna <annakazarez@gmail.com>
2026-09-23 10:18:55 +0300

    refactor: rename method getMastedCard, rename groups regex and extract regex

--- src/main/java/org/company/novysh/security/Main.java
+++ src/main/java/org/company/novysh/security/Main.java
@@ -5,7 +5,7 @@ public class Main {
         String cardNumber = "4111222233334441";
 
         try {
-            String maskedCardNumber = SecurityUtils.getMaskedCard(cardNumber);
+            String maskedCardNumber = SecurityUtils.getMaskedCardNumber(cardNumber);
             System.out.printf("Masked number is %s%n", maskedCardNumber);
         } catch (IllegalArgumentException e) {
             System.out.println("Error masked card: " + e.getMessage());
--- src/main/java/org/company/novysh/security/SecurityUtils.java
+++ src/main/java/org/company/novysh/security/SecurityUtils.java
@@ -3,15 +3,15 @@ package org.company.novysh.security;
 import java.util.Optional;
 
 public class SecurityUtils {
-    private static final String CARD_MASK_REGEX = "(\\d{4})\\d{4}\\d{4}(\\d{4})";
+    private static final String CARD_GROUPS_REGEX = "(\\d{4})\\d{4}\\d{4}(\\d{4})";
+    private static final String SIXTEEN_DIGITS_REGEX = "\\d{16}";
 
-    public static String getMaskedCard(String cardNumber) {
+    public static String getMaskedCardNumber(String cardNumber) {
         return Optional.ofNullable(cardNumber)
-                .filter(str -> str.matches("\\d{16}"))
-                .map(str -> str.replaceAll(CARD_MASK_REGEX, "$1-****-****-$2"))
+                .filter(str -> str.matches(SIXTEEN_DIGITS_REGEX))
+                .map(str -> str.replaceAll(CARD_GROUPS_REGEX, "$1-****-****-$2"))
                 .orElseThrow(() -> new IllegalArgumentException(
                         "Invalid card number format: '" + cardNumber + "'. Expected exactly 16 digits."
                 ));
-
     }
 }
